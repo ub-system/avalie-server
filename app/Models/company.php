be2ae050421b9251abcd179202237d0a14032f83
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-04-04 15:26:48 -0300

    definição das relações de cardinalidade entre as classes

--- app/Models/company.php
+++ app/Models/company.php
@@ -5,7 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class company extends Model
+class Company extends Model
 {
     use HasFactory;
+
+    public function assessments(){
+        return $this->hasMany(Assessment::class);
+    }
 }
